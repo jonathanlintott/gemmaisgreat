b33de8e808a5a0d3a7be1345acaff1fcc0d8ee4e
Jonathan <jonathan_lintott@outlook.com>
2018-03-31 13:10:01 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app>
+
+    <v-content>
+      <router-view/>
+    </v-content>
+
+    <v-footer :fixed="fixed" app>
+      <v-spacer></v-spacer>
+      <span>Gemma is great</span>
+      <v-spacer></v-spacer>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-container fluid>
+    <v-slide-y-transition mode="out-in">
+      <v-layout column align-center>
+        <br/><br/>
+        <h2>Is Gemma Great?</h2>
+        <div>
+          <v-btn @click="message ='You are right!'">Yes</v-btn>
+          <v-btn @click="message = 'Fool! You could not be more wrong'">No</v-btn>
+        </div>
+        <hr/>
+        <br/><br/><br/>
+        <h1>{{ message }}</h1>
+      </v-layout>
+    </v-slide-y-transition>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      message: ''
+    }
+  }
+}
+</script>
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
